ecc11c9c0a6b1b13c23ec356ae7bc8b2bea44294
Tim_B <tblamires22@gmail.com>
2023-02-22 21:58:10 -0700

    Started the project Works in the correct way, however extra funcitonality like the change size still does not work

---
+++ script.js
@@ -0,0 +1,60 @@
+//TODO - allow the user to change size
+//TODO - have the window dynamically resize
+//TODO - make the website look better
+
+
+function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
+
+  function getHeight() {
+    return Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.documentElement.clientHeight
+    );
+  }
+
+const container = document.getElementById("container");
+container.style.width = `${(Math.min(getWidth(), getHeight()) * .9)}px`
+container.style.height = `${(Math.min(getWidth(), getHeight()) * .9)}px`
+
+let gridSize = 16;
+console.log(container);
+makeGrid(gridSize);
+
+
+
+function makeGrid(gridSize) {
+    if(gridSize > 1000)
+        gridSize = 1000;
+
+    //Remove all old elements
+    const oldDiv = document.querySelectorAll("div.pixel");
+    oldDiv.forEach((e) => e.remove);
+
+
+    let divSize = (Math.min(getWidth(), getHeight()) * .9) / gridSize;
+
+    for(let i = 0; i < gridSize ** 2; i++){
+        const element = document.createElement("div");
+        element.id = "pixel";
+        container.appendChild(element);
+        element.addEventListener("mouseover", function() {
+            element.classList.add("currentlyHovered");
+        });
+
+        element.style.width = `${divSize}px`
+        element.style.height = `${divSize}px`
+    }
+
+      
+}
